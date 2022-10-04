7803ee010a0dc6d20034318de7f608c779584842
jolien <j.j.e.vanhooff@gmail.com>
2022-10-04 16:35:59 +0200

    adjusted Max's script to obtain datasets to upload in iTol and applied it in phylogenetic_profiles

--- py_scripts/itol_presabs_matrix.py
+++ py_scripts/itol_presabs_matrix.py
@@ -1,34 +1,32 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--complex', required=True)
-parser.add_argument('-p', '--protein', nargs = '+', required=True)
-parser.add_argument('-col', '--colour', required=True)
+parser = argparse.ArgumentParser(description="make a dataset to upload to iTol for a given protein complex")
+parser.add_argument('-c', '--complex', required=True, help="name of the protein complex, e.g. Condensin I")
+parser.add_argument('-p', '--protein', nargs = '+', required=True, help="name of the proteins constituting the complex and for which there is a phylogenetic profile in the phylogenetic profiles table")
+parser.add_argument('-col', '--colour', required=True, type=str, help="colour for boxes indicating a protein's presence in a taxon; use quotation marks, e.g. '#FFC300'")
+parser.add_argument('-pp', '--pprofile', default="../20220930_condensin_cohesin_SMC56.csv", help="phylogenetics profile comma-separated table")
 
 args = parser.parse_args()
 args = dict(vars(args))
 protein_complex = args.get('complex')
 proteins = list(args.get('protein'))
 colour = args.get('colour')
+eukarya_profiles = pd.read_csv(args.get('pprofile'), index_col='Abbreviation')
+eukarya_species = eukarya_profiles.index.tolist()
 
-##Update pad naar de eukarya_species.tsv
-
-eukarya_species = pd.read_csv('/home/max/NOBINFBACKUP/eukarya5_max/data_set/eukarya_species.tsv', sep = '\t', header = 'infer')
-eukarya_species = eukarya_species['Abbreviation'].tolist()
-
+# Construct strings for writing formatting options to the output file
 field_labels = list()
 for protein in proteins:
     field_labels.append(protein)
 field_labels = '\t'.join(field_labels)
-
 field_colours = '{}\t'.format(colour) * len(proteins)
-print(len(proteins))
 field_shapes = '1\t' * len(proteins)
 
-##Update paden naar 1) waar je je output file wil hebben en 2) waar je lijstjes met ortholog ids staan
-
-with open('itol_presabs/itol_presabs_{}.txt'.format(protein_complex), 'w') as f:
+# Generate dataset textfile (the output)
+with open(f'itol_presabs_{protein_complex}.txt', 'w') as f:
     f.write('DATASET_BINARY\n')
     f.write('SEPARATOR TAB\n')
     f.write('DATASET_LABEL\t{}\n'.format(protein_complex))
@@ -41,18 +39,11 @@ with open('itol_presabs/itol_presabs_{}.txt'.format(protein_complex), 'w') as f:
     f.write('LEGEND_COLORS\t{}\n'.format(field_colours))
     f.write('LEGEND_LABELS\t{}\n'.format(field_labels))
     f.write('DATA\n')
+    # Collect binary results (presence or absence) for each species
     for species in eukarya_species:
-        species_dict = {}
+        protein_value = []
         for protein in proteins:
-            species_dict[protein] = '0'
-            ids = pd.read_csv('all_ids/{}_all_ids.csv'.format(protein), sep = '\t', header = None)
-            ids = ids[0].tolist()
-            for i, seq_id in enumerate(ids):
-                if '_' in seq_id:
-                    seq_id_isolated = seq_id.split('_')[1]
-                    ids[i] = seq_id_isolated
-            ids = [x[0:6] for x in ids]
-            if species in ids:
-                species_dict[protein] = '1'
-        protein_values = '\t'.join(species_dict.values())
-        f.write(species + '\t' + protein_values + '\n')
+            protein_count = eukarya_profiles.loc[species, protein] 
+            protein_value.append('1' if protein_count > 0 else '0')
+        protein_value_string = '\t'.join(protein_value)
+        f.write(species + '\t' + protein_value_string + '\n')
